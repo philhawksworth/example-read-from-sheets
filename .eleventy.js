e161350575156f83fb2d678555778b3f2d026f12
Phil Hawksworth <phil@netlify.com>
2019-08-14 13:23:01 +0100

    scaffolding and data source

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
 
 module.exports = function(config) {
 
@@ -10,31 +8,10 @@ module.exports = function(config) {
   // Layout aliases can make templates more portable
   config.addLayoutAlias('default', 'layouts/base.njk');
 
-  // Add some utility filters
-  config.addFilter("squash", require("./src/utils/filters/squash.js") );
-  config.addFilter("dateDisplay", require("./src/utils/filters/date.js") );
-
-
-  // add support for syntax highlighting
-  config.addPlugin(syntaxHighlight);
-
   // minify the html output
   config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
 
-  // compress and combine js files
-  config.addFilter("jsmin", function(code) {
-    const UglifyJS = require("uglify-js");
-    let minified = UglifyJS.minify(code);
-      if( minified.error ) {
-          console.log("UglifyJS error: ", minified.error);
-          return code;
-      }
-      return minified.code;
-  });
 
-
-  // pass some assets right through
-  config.addPassthroughCopy("./src/site/images");
 
   // make the seed target act like prod
   env = (env=="seed") ? "prod" : env;
@@ -44,10 +21,9 @@ module.exports = function(config) {
       output: "dist",
       data: `_data/${env}`
     },
-    templateFormats : ["njk", "md", "11ty.js"],
+    templateFormats : ["njk", "md"],
     htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk",
-    passthroughFileCopy: true
+    markdownTemplateEngine : "njk"
   };
 
 };
---
+++ src/site/_data/prod/sheet.js
@@ -0,0 +1,42 @@
+const axios  = require('axios');
+const seed   = require('../../../utils/save-seed.js');
+
+
+// Once a googel sheet is "published to the web" we can access its JSON
+// via a URL of this form. We just need to pass in the ID of the sheet
+// which we can find in the URL of the document.
+const sheetID = "1CfI6XGm9OjjNKGr3kXRSKVLui_gkHZdadoOPIiNgE9s";
+const googleSheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
+
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(googleSheetUrl)
+      .then(response => {
+
+        // massage the data from the Google Sheets API into
+        // a shape that will more convenient for us in our SSG.
+        var data = [];
+        response.data.feed.entry.forEach(item => {
+          data.push({
+            "name": item.gsx$name.$t,
+            "number": item.gsx$number.$t
+          });
+        });
+
+        // stash the data locally for developing without
+        // needing to hit the API each time.
+        seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`)
+
+        // resolve the promise and return the data
+        resolve(data);
+
+      })
+
+      // uh-oh. Handle any errrors we might encounter
+      .catch(error => {
+        console.log('Error :', error);
+        reject(error);
+      });
+  })
+}
